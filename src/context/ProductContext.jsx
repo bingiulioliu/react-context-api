19bd48d372bd30011174191924bc93bc04b392d2
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-14 16:33:08 +0200

    FEAT dummy context

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,2 +1,13 @@
 import { createContext } from "react";
 
+const ProductContext = createContext(null);
+
+function ProductProvider({children}){
+    const data = 0;
+
+    return <>
+        <ProductContext value={data}>
+            {children}
+        </ProductContext>
+    </>
+}
