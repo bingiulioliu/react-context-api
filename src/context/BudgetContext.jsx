03eb9d25dfc4a962f49f51e05393044e674f9989
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-14 18:25:43 +0200

    Importo i valori da useProducts a BudgetContext

--- src/context/BudgetContext.jsx
+++ src/context/BudgetContext.jsx
@@ -1,10 +1,14 @@
 import { createContext, useState } from "react";
+import { useProducts } from "../hooks/useProducts";
+
 
 export const BudgetContext = createContext(null);
 
 export function BudgetProvider({children}){
 
     const [isPoorMode, setIsPoorMode] = useState(false);
+
+    const {filter, filteredProducts: filteredTemp, handleChange, categories} = useProducts();
 
     const togglePoorMode = () => {
         setIsPoorMode(isPoorMode ? false : true);
